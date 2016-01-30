dfdc9a67316116ee54f67a214bc8efabf8b3edd4
Jonte <jonathan.bosson@gmail.com>
2016-01-30 18:58:34 +0100

    dFdx extension now worked, herp. nice im done

--- shaders/waterFragmentShader.glsl
+++ shaders/waterFragmentShader.glsl
@@ -1,5 +1,7 @@
 precision mediump float;
 
+#extension GL_OES_standard_derivatives : enable
+
 varying vec4 vPosition;
 varying vec4 vNormal;
 varying vec3 pos;
@@ -15,10 +17,9 @@ void main()
 	vec3 finalColor = vec3(0.1, 0.2, 0.4);
 
 	// Blinn Phong Shading
-  //vec3 dx = dFdx(vec3(vPosition));
+  vec3 dx = dFdx(vec3(vPosition));
   //vec3 dy = dFdy(vec3(vPosition));
-	vec3 newNormal = normalize(vec3(vNormal));
-  //normalize(cross(dFdx(vec3(vNormal)), dFdy(vec3(vNormal))));
+	vec3 newNormal = normalize(cross(dFdx(vec3(vNormal)), dFdy(vec3(vNormal))));//normalize(vec3(vNormal));
   vec3 lightDir = normalize(lightPos - vec3(vPosition));
 
   float lambertian = max(dot(lightDir, newNormal), 0.0);
